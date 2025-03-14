ccad323ab31b6a87cfc41fb60b6ea8761cc3509e
Kamal Nayan <geniusamansingh@gmail.com>
2025-03-15 04:12:09 +0530

    building wallet components

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,29 +5,18 @@ import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { ConnectButton } from "./ConnectButton"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
+
+          <ConnectButton />
+
           <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
             <Link
               href={siteConfig.links.twitter}
               target="_blank"
@@ -39,11 +28,12 @@ export function SiteHeader() {
                   variant: "ghost",
                 })}
               >
-                <Icons.twitter className="h-4 w-4 fill-current" />
+                <Icons.twitter className="size-4 fill-current" />
                 <span className="sr-only">Twitter</span>
               </div>
             </Link>
             <ThemeToggle />
+
           </nav>
         </div>
       </div>
